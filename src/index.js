e983d290a625afc01a0151ac13d94a1057520bc9
thai.phan <thaiphan2712@gmail.com>
2017-09-08 08:16:02 +0700

    generator code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import Game from './Game';
 import registerServiceWorker from './registerServiceWorker';
+import * as code from 'voucher-code-generator';
 
+
+let mycode = code.generate({
+    length: 5,
+})
+console.log(mycode);
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Game />, document.getElementById('game'));
 registerServiceWorker();
